74558993bb709a6115043c4891c69708f1ecc00d
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2023-03-29 00:03:07 +0500

    Class or init.

---
+++ 04_getter_setter_and_more/dist/app.js
@@ -0,0 +1,38 @@
+"use strict";
+// Class 
+// class Student {
+//      name: string;
+//     rollNum: number;
+//     constructor(n: string,rn:number){
+//         this.name = n;
+//         this.rollNum = rn;
+//     }
+// }
+// Shorthand way
+class Student {
+    constructor(name, rollNum) {
+        this.name = name;
+        this.rollNum = rollNum;
+        this.skills = [];
+    }
+    addSkill(skill) {
+        this.skills.push(skill);
+    }
+    getSkills() {
+        return this.skills;
+    }
+}
+const student1 = new Student("Muhammad Huzaifa Khan", 1234);
+// student1.rollNum = 123 // not allow because of readonly
+// student1.skills.push("Flutter");  // not allowed when make array private
+// student1.skills.push("NodeJS");
+student1.addSkill("JavaScript");
+student1.addSkill("Flutter");
+student1.addSkill("NodeJS");
+console.log(student1);
+console.log(student1.getSkills());
+// Access Modifiers
+// public (by default)
+// private
+// readonly => only allow to get data, not write or change the data.
+//# sourceMappingURL=app.js.map
