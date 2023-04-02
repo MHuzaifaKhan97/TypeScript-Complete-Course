91f2ed5efd585381db6e6610259542b4bce0b474
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2023-04-02 20:27:11 +0500

    Add files via upload

---
+++ 06_class_work/src/app.ts
@@ -0,0 +1,50 @@
+// Type Casting
+
+// const input = document.getElementById("input1") as HTMLInputElement;
+// or 
+// const input = <HTMLInputElement>document.getElementById("input1");
+
+// Generics
+
+// function promise () : Promise<string> {
+//   return new Promise((resolve,reject) => {
+//       setTimeout(()=> {
+//          resolve("Task performed successfully");
+//       },2000)
+//    }); 
+// }
+
+// promise().then((data) => {
+//    const array= data.split(" ");
+//    console.log(array);
+// })
+
+// type User  = {
+//    name: string,
+//    id: number,
+// }
+// function promise () : Promise<User> {
+//    return new Promise((resolve,reject) => {
+//        setTimeout(()=> {
+//           resolve({
+//             name:"Huzaifa Khan",
+//             id:1234
+//           });
+//        },2000)
+//     }); 
+//  }
+ 
+//  promise().then((data) => {
+//     console.log(data.name);
+//  })
+
+function merge<T extends object,U extends object>(objA :T, objB: U) {
+   return {...objA, ...objB};
+}
+
+// const merged = merge({"name":"Huzaifa"},{id:1234}) 
+// the problem using <T,U> is k koi bh value ko wo allow kardega e.g:
+const merged = merge({"name":"Huzaifa"},1234)
+//  to avoid this 
+
+console.log(merge.name);
